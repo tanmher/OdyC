48e17d87f622fa365c2d46928cdce37ee0159f3a
mher <tanmher09@gmail.com>
2021-12-11 01:56:50 +0800

    fixed tab error

--- main/lexical.py
+++ main/lexical.py
@@ -6,7 +6,7 @@ class Token(NamedTuple):
     value: str
     line: int
     column: int
-    error: list
+    error: str
 
 def tokenize(code):
     keywords = {"boolean", "boot","break", "continue", "digit","if", "elif", "else",
@@ -15,16 +15,18 @@ def tokenize(code):
                 "trojan", "TRUE","void", "while"
     }
 
-    reserved_symbols = {'+', '-', '*', '^', '/', '//', '%', '=', '+=', '-=', '*=', '^=', '/=', '//=', '%=', '=', '!=', '>', '<', '>=', '<=','&&',
-                        '||', '!', '++', '--', '(', ')', '#', '[', ']', ':'
-    }
+    # reserved_symbols = {'+', '-', '*', '^', '/', '//', '%', '=', '+=', '-=', '*=', '^=', '/=', '//=', '%=', '=', '!=', '>', '<', '>=', '<=','&&',
+    #                     '||', '!', '++', '--', '(', ')', '#', '[', ']', ':'
+    # }
 
     token_specification = [
         ('functions',   r'[A-Za-z0-9]+\(\)'),       # Functions
         ('digits',      r'\d+(\.\d*)?'),            # Integer or decimal number
         ('id',          r'[A-Za-z0-9]+'),           # Identifiers
-        ('string',      r'\"[ -~][ -~]+\"'),        # String
-        ('newline',     r'\n'),                     # Line endings
+        ('string',      r'\"[ -~][ -~]+\"'),        # String Literals
+        ('operator',    r'[+\-*^/%&\|=\(\)]+'),     # Operators
+        ('start',       r':+(\n\t+)+'),             # Start of code block 
+        ('newline',     r'\n'),                     # New line
         ('whitespace',  r'[ \t]+'),                 # Skip over spaces and tabs
         ('mismatch',    r'.'),                      # Any other character
     ]
@@ -33,13 +35,18 @@ def tokenize(code):
     line_num = 1
     count = 0
     list_id = {}
-    error = []
+    line_start = 0
+    error = ''
+    cb_count = 0
+    newline_count = 0
+    indent_count = []
     for mo in re.finditer(tok_regex, code):
         kind = mo.lastgroup
         value = mo.group()
-        column = mo.span()
+        column = mo.start() - line_start
         if kind == 'functions':
             kind = value
+            indent_count.append(0) #FOR NOW TO SOLVE ERROR IN USER DEFINED FUNCTIONS
         elif kind == 'digits':
             if len(value) <= 9 and len(value) >= 1 and '.' not in value:
                 kind = 'int_lit'
@@ -48,14 +55,12 @@ def tokenize(code):
                 kind = 'dec_lit'
                 value = float(value)
             else:
-                error.append("Lexical Error at Line: "+ str(line_num) +  " Column: " + str(column) + ": Integer/Decimal is out of bounds")
+                error = f'Lexical Error at Line: {line_num}  Column: {column} : Integer/Decimal is out of bounds'
                 kind = 'invalid'
-                value = 'invalid'
         elif kind == 'id':
             if len(value) > 20:
-                error.append("Lexical Error at Line "+ str(line_num) +  " Column" + str(column) + ": Maximum character for identifiers is 20")
+                error = f'Lexical Error at Line: {line_num} Column: {column}: Maximum character for identifiers is 20'
                 kind = 'invalid'
-                value = 'invalid'
             elif value in keywords:
                 kind = value
             elif value in list_id.values():
@@ -67,14 +72,40 @@ def tokenize(code):
                 count+=1
                 kind = 'id' + str(count)
                 list_id[kind] = value 
-        elif value in reserved_symbols:
-            kind = value
+        elif kind == 'operator':
+            if len(value) > 2:
+                error = f'Lexical Error at Line: {line_num} Column: {column}: Invalid operator'
+                kind = 'invalid'
+            else:
+                kind = value
+        elif kind == 'start':
+            cb_count += 1
+            newline_count += 1
+            tab_count = value.count('\t')
+            indent_count.append(tab_count)
+            print(indent_count)
+            print(line_num)
+            print(tab_count)
+            if '\n' in value and '\t' in value and tab_count == indent_count[line_num-1]+1:
+                kind = ':'
+                value = ':'
+                line_num += 1
+            else:
+                error = f'Lexical Error at Line: {line_num} Column: {column}: Block must be in newline and indented'
+                kind = 'invalid'
+                value = ':'
         elif kind == 'newline':
+            line_start = mo.end()
             line_num += 1
             continue
         elif kind == 'whitespace':
+            if '\t' in value:
+                tab_count = value.count('\t')
+                indent_count.append(tab_count)
             value = 'whitespace'
         elif kind == 'mismatch':
-            raise RuntimeError(f'{value!r} unexpected on line {line_num}')
+            error = f'Lexical Error at Line: {line_num} Column: {column}: Unexpected value'
+            kind = 'invalid'
+            # raise RuntimeError(f'{value} unexpected on line {line_num}')
 
         yield Token(kind, value, line_num, column, error)
